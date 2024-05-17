79b80cf3c033aec82baab086c242d6d9b4850778
Cycatz <thesummernightcat@gmail.com>
2024-05-18 00:12:03 +0800

    Build the project with LLVM toolchain

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,4 +1,7 @@
 set(CMAKE_SYSTEM_NAME Generic)
 
-set(TOOLCHAIN_PREFIX riscv64-unknown-elf-)
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_C_COMPILER "clang")
+set(CMAKE_C_FLAGS "-target riscv64-unknown-elf")
+set(CMAKE_ASM_COMPILER "clang")
+set(CMAKE_ASM_FLAGS "-target riscv64-unknown-elf")
